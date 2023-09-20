2eaa8c10e436f3ae8f6edbac0330cae9dd12db91
Noyan Kucukgultekin <Noyan.Kucukgultekin@d-teknoloji.com.tr>
2023-09-20 14:47:56 +0300

    added to github

---
+++ C#/ImageEdgeDetection/Matrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/// <Özet>
+/// Sobel Matrisinin Tanımlandığı sınıf
+/// </Özet>
+namespace ImageEdgeDetection
+{
+    public static class Matrix
+    {
+        public static double[,] Sobel3x3Horizontal
+        {
+            get
+            {
+                return new double[,]
+                { { -1,  0,  1, },
+                  { -2,  0,  2, },
+                  { -1,  0,  1, }, };
+            }
+        }
+
+        public static double[,] Sobel3x3Vertical
+        {
+            get
+            {
+                return new double[,]
+                { {  1,  2,  1, },
+                  {  0,  0,  0, },
+                  { -1, -2, -1, }, };
+            }
+        }
+
+    }
+}
---
+++ C#/ImageEdgeDetection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+/// <Özet>
+/// Programın Koşulmaya Başlandığı ilk sınıf
+/// </Özet>
+namespace ImageEdgeDetection
+{
+    static class Program
+    {
+ 
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
